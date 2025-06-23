e5d56c7153a07023e8883c7fdddaf0e11c173be7
gitsetu <pedroroyo@hotmail.com>
2025-06-23 23:25:14 +0100

    New button for Upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -11,7 +11,7 @@ import MovieFilterUI, {
 import { UpcomingMovies } from "../types/interfaces";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToPlaylistIcon from '../components/cardIcons/playlistAdd'
 
 
 const titleFiltering = {
@@ -63,7 +63,7 @@ const UpcomingMoviesPage: React.FC = () => {
         title="Upcoming Movies"
         movies={displayedMovies}
         action={(movie: BaseMovieProps) => {
-          return <AddToFavouritesIcon {...movie} />
+          return <AddToPlaylistIcon {...movie} />
         }}
       />
       <MovieFilterUI
